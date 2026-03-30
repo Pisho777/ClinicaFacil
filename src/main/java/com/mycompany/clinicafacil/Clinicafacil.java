4823b286fc41368523def1ec1f5b05c159518d7f
Pisho777 <higorfgoncalves784@gmail.com>
2026-03-30 16:35:26 -0300

    adiciona classes iniciais do modelo, controller e util (estrutura base do sistema)

---
+++ src/main/java/com/mycompany/clinicafacil/Clinicafacil.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clinicafacil;
+
+/**
+ *
+ * @author Higor
+ */
+public class Clinicafacil {
+
+    public static void main(String[] args) {
+        System.out.println("Burn World!");
+    }
+}
